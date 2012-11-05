729dfe741f539fc1e06f6fb72809179b32da547b
xinyuepeng <bugboy_bugboy@hotmail.com>
2012-11-05 16:45:48 +0800

    Update main.c

--- Tests/testB/main.c
+++ Tests/testB/main.c
@@ -1,4 +1,4 @@
-#if 1
+#if 0
 //******************************************************************************
 //  MSP-FET430P140 Demo - Timer_B, Toggle P1.0, CCR0 Cont. Mode ISR, DCO SMCLK
 //
@@ -28,10 +28,13 @@
 void main(void)
 {
   WDTCTL = WDTPW + WDTHOLD;                 // Stop WDT
+  
+  BCSCTL2 = 0;                //MCLK = DCOCLK, SMCLK=DCOCLK, = EXT CRYTAL 32.768KHZ
+  
   P4DIR |= 0x01;                            // P4.0 output
   TBCCTL0 = CCIE;                           // CCR0 interrupt enabled
   TBCCR0 = 50000;
-  TBCTL = TBSSEL_2 + MC_2;                  // SMCLK, contmode
+  TBCTL = TBSSEL_1 + MC_2;                  // SMCLK, contmode
 
   _BIS_SR(LPM0_bits + GIE);                 // Enter LPM0 w/ interrupt
 }
@@ -42,5 +45,50 @@ __interrupt void Timer_B (void)
 {
   P4OUT ^= 0x01;                            // Toggle P4.0
   TBCCR0 += 50000;                          // Add Offset to CCR0
+}
+
+#endif
+
+#if 1
+//******************************************************************************
+//  MSP-FET430P140 Demo - Timer_B, Toggle P1.0, CCR0 Up Mode ISR, DCO SMCLK
+//
+//  Description: Toggle P1.0 using software and TB_0 ISR. Timer_B is
+//  configured for up mode, thus the timer overflows when TBR counts
+//  to CCR0. In this example, CCR0 is loaded with 20000.
+//  ACLK = n/a, MCLK = SMCLK = TBCLK = default DCO ~800kHz
+//
+//           MSP430F149
+//         ---------------
+//     /|\|            XIN|-
+//      | |               |
+//      --|RST        XOUT|-
+//        |               |
+//        |           P4.0|-->LED
+//
+//  M. Buccini / G. Morton
+//  Texas Instruments Inc.
+//  May 2005
+//  Built with Code Composer Essentials Version: 1.0
+//******************************************************************************
+
+#include  <msp430x14x.h>
+
+void main(void)
+{
+  WDTCTL = WDTPW + WDTHOLD;                 // Stop WDT
+  P4DIR |= 0x01;                            // Set P1.0 to output direction
+  TBCCTL0 = CCIE;                           // TBCCR0 interrupt enabled
+  TBCCR0 = 20000;
+  TBCTL = TBSSEL_2 + MC_1;                  // SMCLK, upmode
+
+  _BIS_SR(LPM0_bits + GIE);                 // Enter LPM0 w/ interrupt
+}
+
+// Timer B0 interrupt service routine
+#pragma vector = TIMERB0_VECTOR
+__interrupt void Timer_B (void)
+{
+  P4OUT ^= 0x01;                            // Toggle P1.0 using exclusive-OR
 }
 #endif
