cd4d13999a9f4cc8c06363f5307e47158fa3301b
xinyuepeng <bugboy_bugboy@hotmail.com>
2012-11-08 16:48:06 +0800

    Update timerB test code

--- Tests/testB/main.c
+++ Tests/testB/main.c
@@ -93,37 +93,23 @@ __interrupt void Timer_B (void)
 }
 #endif
 
-#if 1
+#if 0
 //******************************************************************************
-//  MSP-FET430P140 Demo - Timer_A, Toggle P1.0-3, Cont. Mode ISR, DCO SMCLK
+//  MSP-FET430P140 Demo - Timer_B, Toggle P4.0, Overflow ISR, DCO SMCLK
 //
-//  Description: Use Timer_A CCRx units and overflow to generate four
-//  independent timing intervals. For demonstration, CCR0, CCR1 and CCR2
-//  output units are optionally selected with port pins P1.1, P1.2 and P1.3
-//  in toggle mode. As such, these pins will toggle when respective CCRx
-//  registers match the TAR counter. Interrupts are also enabled with all
-//  CCRx units, software loads offset to next interval only - as long as the
-//  interval offset is added to CCRx, toggle rate is generated in hardware.
-//  Timer_A overflow ISR is used to toggle P1.0 with software. Proper use of
-//  the TAIV interrupt vector generator is demonstrated.
-//  ACLK = n/a, MCLK = SMCLK = TACLK = default DCO ~800kHz
+//  Description: Toggle P4.0 using software and Timer_B overflow ISR.
+//  In this example an ISR triggers when TB overflows.  Inside the TB
+//  overflow ISR P4.0 is toggled. Toggle rate is approximatlely 12Hz.
+//  Proper use of the TBIV interrupt vector generator is demonstrated.
+//  ACLK = n/a, MCLK = SMCLK = TBCLK = default DCO ~800kHz.
 //
-//  As coded with TACLK ~800kHz DCO, toggle rates are:
-//  P1.1= CCR0 = 800kHz/(2*200) ~2000Hz
-//  P1.2= CCR1 = 800kHz/(2*1000) ~400Hz
-//  P1.3= CCR2 = 800kHz/(2*10000) ~40Hz
-//  P1.0= overflow = 800kHz/(2*65536) ~6Hz
-//
-//               MSP430F149
-//            -----------------
-//        /|\|              XIN|-
-//         | |                 |
-//         --|RST          XOUT|-
-//           |                 |
-//           |         P1.1/TA0|--> CCR0
-//           |         P1.2/TA1|--> CCR1
-//           |         P1.3/TA2|--> CCR2
-//           |             P1.0|--> Overflow/software
+//           MSP430F149
+//         ---------------
+//     /|\|            XIN|-
+//      | |               |
+//      --|RST        XOUT|-
+//        |               |
+//        |           P4.0|-->LED
 //
 //  M. Buccini / G. Morton
 //  Texas Instruments Inc.
@@ -136,37 +122,452 @@ __interrupt void Timer_B (void)
 void main(void)
 {
   WDTCTL = WDTPW + WDTHOLD;                 // Stop WDT
-  P1SEL |= 0x0E;                            // P1.1 - P1.4 option select
-  P1DIR |= 0x0F;                            // P1.0 - P1.4 outputs
-  CCTL0 = OUTMOD_4 + CCIE;                  // CCR0 toggle, interrupt enabled
-  CCTL1 = OUTMOD_4 + CCIE;                  // CCR1 toggle, interrupt enabled
-  CCTL2 = OUTMOD_4 + CCIE;                  // CCR2 toggle, interrupt enabled
-  TACTL = TASSEL_2 +  MC_2 + TAIE;          // SMCLK, Contmode, int enabled
+  P4DIR |= 0x01;                            // Set P4.0 to output direction
+  TBCTL = TBSSEL_2 + MC_2 + TBIE;           // SMCLK, contmode, interrupt
 
   _BIS_SR(LPM0_bits + GIE);                 // Enter LPM0 w/ interrupt
 }
 
-// Timer A0 interrupt service routine
-__interrupt void Timer_A0 (void);
-TIMERA0_ISR(Timer_A0)
-__interrupt void Timer_A0 (void)
+// Timer_B7 Interrupt Vector (TBIV) handler
+#pragma vector = TIMERB1_VECTOR
+__interrupt void Timer_B(void)
 {
-  CCR0 += 200;                              // Add Offset to CCR0
-}
-
-// Timer_A3 Interrupt Vector (TAIV) handler
-__interrupt void Timer_A1(void);
-TIMERA1_ISR(Timer_A1)
-__interrupt void Timer_A1(void)
-{
-  switch( TAIV )
-  {
-  case  2: CCR1 += 1000;                    // Add Offset to CCR1
-           break;
-  case  4: CCR2 += 10000;                   // Add Offset to CCR2
-           break;
-  case 10: P1OUT ^= 0x01;                   // Timer_A3 overflow
-           break;
+ switch( TBIV )
+ {
+   case  2: break;                          // CCR1 not used
+   case  4: break;                          // CCR2 not used
+   case 14: P4OUT ^= 0x01;                  // overflow
+            break;
  }
 }
+
+
+#endif
+
+#if 0
+//******************************************************************************
+//  MSP-FET430P140 Demo - Timer_B, Toggle P4.0, Overflow ISR, 32kHz ACLK
+//
+//  Description: Toggle P4.0 using software and the Timer_B overflow ISR.
+//  In this example an ISR triggers when TB overflows. Inside the ISR P1.0
+//  is toggled. Toggle rate is exactly 0.5Hz. Proper use of the TBIV
+//  interrupt vector generator is demonstrated.
+//  ACLK = TBCLK = 32kHz, MCLK = SMCLK = default DCO ~800kHz
+//  //* An external watch crystal on XIN XOUT is required for ACLK *//	
+//
+//           MSP430F149
+//         ---------------
+//     /|\|            XIN|-
+//      | |               | 32kHz
+//      --|RST        XOUT|-
+//        |               |
+//        |           P4.0|-->LED
+//
+//  M. Buccini / G. Morton
+//  Texas Instruments Inc.
+//  May 2005
+//  Built with Code Composer Essentials Version: 1.0
+//******************************************************************************
+
+#include  <msp430x14x.h>
+
+void main(void)
+{
+  WDTCTL = WDTPW + WDTHOLD;                 // Stop WDT
+  P4DIR |= 0x01;                            // Set P4.0 to output direction
+  TBCTL = TBSSEL_1 + MC_2 + TBIE;           // ACLK, contmode, interrupt
+
+  _BIS_SR(LPM3_bits + GIE);                 // Enter LPM3 w/ interrupt
+}
+
+// Timer_B7 Interrupt Vector (TBIV) handler
+#pragma vector = TIMERB1_VECTOR
+__interrupt void Timer_B(void)
+{
+ switch( TBIV )
+ {
+   case  2: break;                          // CCR1 not used
+   case  4: break;                          // CCR2 not used
+   case 14: P4OUT ^= 0x01;                  // overflow
+           break;
+  }
+}
+
+#endif
+
+#if 0
+//*****************************************************************************
+//  MSP-FET430P140 Demo - Timer_B, Toggle P1.0, CCR0 Up Mode ISR, 32kHz ACLK
+//
+//  Description: Toggle P1.0 using software and the TB_0 ISR. Timer_B is
+//  configured for up mode, thus the timer overflows when TBR counts
+//  to CCR0. In this example, CCR0 is loaded with 1000-1.
+//  Toggle rate = 32768Hz/(2*1000) = 16.384Hz
+//  ACLK = TBCLK = 32kHz, MCLK = SMCLK = default DCO ~800kHz
+//  //* An external watch crystal on XIN XOUT is required for ACLK *//	
+//
+//           MSP430F149
+//         ---------------
+//     /|\|            XIN|-
+//      | |               | 32kHz
+//      --|RST        XOUT|-
+//        |               |
+//        |           P1.0|-->LED
+//
+//  M. Buccini / G. Morton
+//  Texas Instruments Inc.
+//  May 2005
+//  Built with Code Composer Essentials Version: 1.0
+//*****************************************************************************
+
+#include  <msp430x14x.h>
+
+void main(void)
+{
+  WDTCTL = WDTPW + WDTHOLD;                 // Stop WDT
+  P4DIR |= 0x01;                            // Set P1.0 to output direction
+  TBCTL = TBSSEL_1 + TBCLR;                 // ACLK, clear TBR
+  TBCCTL0 = CCIE;                           // TRCCR0 interrupt enabled
+  TBCCR0 = 1000-1;
+  TBCTL = TBSSEL_1 + MC_1;                  // ACLK, upmode
+
+  _BIS_SR(LPM3_bits + GIE);                 // Enter LPM3 w/ interrupt
+}
+
+// Timer B0 interrupt service routine
+#pragma vector = TIMERB0_VECTOR
+__interrupt void Timer_B (void)
+{
+    P4OUT ^= 0x01;                          // Toggle P1.0 using exclusive-OR
+}
+
+
+#endif
+
+#if 0
+//*******************************************************************************
+//  MSP-FET430P140 Demo - Timer_B, PWM TB1-6, Up Mode, DCO SMCLK
+//
+//  Description: This program generates six PWM outputs on P4.1-6 using
+//  Timer_B configured for up mode. The value in CCR0, 512-1, defines the PWM
+//  period and the values in CCR1-6 the PWM duty cycles. Using ~800kHz SMCLK
+//  as TBCLK, the timer period is ~640us.
+//  ACLK = na, SMCLK = MCLK = TBCLK = default DCO ~800kHz.
+//
+//               MSP430F149
+//            -----------------
+//        /|\|              XIN|-
+//         | |                 |
+//         --|RST          XOUT|-
+//           |                 |
+//           |         P4.1/TB1|--> CCR1 - 75% PWM
+//           |         P4.2/TB2|--> CCR2 - 25% PWM
+//           |         P4.3/TB3|--> CCR3 - 12.5% PWM
+//           |         P4.4/TB4|--> CCR4 - 6.25% PWM
+//           |         P4.5/TB5|--> CCR5 - 3.125% PWM
+//           |         P4.6/TB6|--> CCR6 - 1.5625% PWM
+//
+//  M. Buccini / G. Morton
+//  Texas Instruments Inc.
+//  May 2005
+//  Built with Code Composer Essentials Version: 1.0
+//******************************************************************************
+
+#include  <msp430x14x.h>
+
+void main(void)
+{
+  WDTCTL = WDTPW + WDTHOLD;                 // Stop WDT
+  P4DIR |= 0x7e;                            // P4.1 - P4.6 output
+  P4SEL |= 0x7e;                            // P4.1 - P4.6 TBx options
+  TBCCR0 = 512-1;                           // PWM Period
+  TBCCTL1 = OUTMOD_7;                       // CCR1 reset/set
+  TBCCR1 = 384;                             // CCR1 PWM duty cycle
+  TBCCTL2 = OUTMOD_7;
+  TBCCR2 = 128;
+  TBCCTL3 = OUTMOD_7;
+  TBCCR3 = 64;
+  TBCCTL4 = OUTMOD_7;
+  TBCCR4 = 32;
+  TBCCTL5 = OUTMOD_7;
+  TBCCR5 = 16;
+  TBCCTL6 = OUTMOD_7;
+  TBCCR6 = 8;
+  TBCTL = TBSSEL_2 + MC_1;                  // SMCLK, up mode
+
+  _BIS_SR(CPUOFF);                          // Enter LPM0
+}
+
+
+#endif
+
+#if 0
+//*******************************************************************************
+//  MSP-FET430P140 Demo - Timer_B, PWM TB1-6, Up Mode, 32kHz ACLK
+//
+//  Description: This program generates six PWM outputs on P4.1-6 using
+//  Timer_B configured for up mode. The value in CCR0, 512-1, defines the PWM
+//  period and the values in CCR1-6 the PWM duty cycles. Using 32kHz ACLK
+//  as TBCLK, the timer period is 15.6ms. Normal operating mode is LPM3.
+//  ACLK = TBCLK = LFXT1 = 32768Hz, MCLK = SMCLK = default DCO ~800kHz.
+//  //* External watch crystal installed on XIN XOUT is required for ACLK *//	
+//
+//               MSP430F149
+//            -----------------
+//        /|\|              XIN|-
+//         | |                 | 32kHz
+//         --|RST          XOUT|-
+//           |                 |
+//           |         P4.1/TB1|--> CCR1 - 75% PWM
+//           |         P4.2/TB2|--> CCR2 - 25% PWM
+//           |         P4.3/TB3|--> CCR3 - 12.5% PWM
+//           |         P4.4/TB4|--> CCR4 - 6.25% PWM
+//           |         P4.5/TB5|--> CCR5 - 3.125% PWM
+//           |         P4.6/TB6|--> CCR6 - 1.5625% PWM
+//
+//  M. Buccini / G. Morton
+//  Texas Instruments Inc.
+//  May 2005
+//  Built with Code Composer Essentials Version: 1.0
+//******************************************************************************
+
+#include  <msp430x14x.h>
+
+void main(void)
+{
+  WDTCTL = WDTPW + WDTHOLD;                 // Stop WDT
+  P4DIR |= 0x7e;                            // P4.1 - P4.6 output
+  P4SEL |= 0x7e;                            // P4.1 - P4.6 TBx options
+  TBCCR0 = 512-1;                           // PWM Period
+  TBCCTL1 = OUTMOD_7;                       // CCR1 reset/set
+  TBCCR1 = 384;                             // CCR1 PWM duty cycle
+  TBCCTL2 = OUTMOD_7;
+  TBCCR2 = 128;
+  TBCCTL3 = OUTMOD_7;
+  TBCCR3 = 64;
+  TBCCTL4 = OUTMOD_7;
+  TBCCR4 = 32;
+  TBCCTL5 = OUTMOD_7;
+  TBCCR5 = 16;
+  TBCCTL6 = OUTMOD_7;
+  TBCCR6 = 8;
+
+  TBCTL = TBSSEL_1 + MC_1;                  // ACLK, up mode
+
+  _BIS_SR(LPM3_bits);                       // Enter LPM3
+}
+
+
+#endif
+
+#if 0// Cannot test now
+
+//*******************************************************************************
+//  MSP-FET430P140 Demo - Timer_B, PWM TB1-2, Up Mode, HF XTAL ACLK
+//
+//  Description: This program generates two PWM outputs on P4.1,2 using
+//  Timer_B configured for up mode. The value in CCR0, 512-1, defines the PWM
+//  period and the values in CCR1 and CCR2 the PWM duty cycles. Using HF XTAL
+//  ACLK as TBCLK, the timer period is HF XTAL/512 with a 75% duty cycle
+//  on P4.1 and 25% on P4.2.
+//  ACLK = MCLK = TBCLK = HF XTAL
+//  //* HF XTAL REQUIRED AND NOT INSTALLED ON FET *//
+//  //* Min Vcc required varies with MCLK frequency - refer to datasheet *//	
+//
+//               MSP430F149
+//            -----------------
+//        /|\|              XIN|-
+//         | |                 | HF XTAL (455k - 8MHz)
+//         --|RST          XOUT|-
+//           |                 |
+//           |         P4.1/TB1|--> CCR1 - 75% PWM
+//           |         P4.2/TB2|--> CCR2 - 25% PWM
+//
+//  M. Buccini / G. Morton
+//  Texas Instruments Inc.
+//  May 2005
+//  Built with Code Composer Essentials Version: 1.0
+//******************************************************************************
+
+#include  <msp430x14x.h>
+
+void main(void)
+{
+  volatile unsigned int i;
+  WDTCTL = WDTPW + WDTHOLD;                 // Stop WDT
+  BCSCTL1 |= XTS;                           // ACLK = LFXT1 = HF XTAL
+
+  do
+  {
+    IFG1 &= ~OFIFG;                         // Clear OSCFault flag
+    for (i = 0xFF; i > 0; i--);             // Time for flag to set
+  }
+  while ((IFG1 & OFIFG));                   // OSCFault flag still set?
+
+  BCSCTL2 |= SELM_3;                        // MCLK= LFXT1 (safe)
+  P4DIR |= 0x06;                            // P4.1 and P4.2 output
+  P4SEL |= 0x06;                            // P4.2 and P4.2 TB1/2 options
+  TBCCR0 = 512-1;                           // PWM Period
+  TBCCTL1 = OUTMOD_7;  			                // CCR1 reset/set
+  TBCCR1 = 384;                             // CCR1 PWM duty cycle
+  TBCCTL2 = OUTMOD_7;  			                // CCR2 reset/set
+  TBCCR2 = 128;                             // CCR2 PWM duty cycle
+  TBCTL = TBSSEL_1 + MC_1;                  // ACLK, up mode
+
+  _BIS_SR(CPUOFF);                          // Enter LPM0
+}
+
+
+#endif
+
+#if 0
+//*******************************************************************************
+//  MSP-FET430P140 Demo - Timer_B, PWM TB1-2, Up/Down Mode, DCO SMCLK
+//
+//  Description: This program generates two PWM outputs on P4.1,2 using
+//  Timer_B configured for up/down mode. The value in CCR0, 128, defines the
+//  PWM period/2 and the values in CCR1 and CCR2 the PWM duty cycles. Using
+//  ~800kHz SMCLK as TBCLK, the timer period is ~320us with a 75% duty cycle
+//  on P4.1 and 25% on P4.2.
+//  SMCLK = MCLK = TBCLK = default DCO ~800kHz.
+//
+//               MSP430F149
+//            -----------------
+//        /|\|              XIN|-
+//         | |                 |
+//         --|RST          XOUT|-
+//           |                 |
+//           |         P4.1/TB1|--> CCR1 - 75% PWM
+//           |         P4.2/TB2|--> CCR2 - 25% PWM
+//
+//  M. Buccini / G. Morton
+//  Texas Instruments Inc.
+//  May 2005
+//  Built with Code Composer Essentials Version: 1.0
+//  (128-32)*2/(128*2)
+//******************************************************************************
+
+#include  <msp430x14x.h>
+
+void main(void)
+{
+  WDTCTL = WDTPW + WDTHOLD;                 // Stop WDT
+  P4DIR |= 0x06;                            // P4.1 and P4.2 output
+  P4SEL |= 0x06;                            // P4.2 and P4.2 TB1/2 otions
+  TBCCR0 = 128;                             // PWM Period/2
+  TBCCTL1 = OUTMOD_6;                       // CCR1 toggle/set
+  TBCCR1 = 32;                              // CCR1 PWM duty cycle
+  TBCCTL2 = OUTMOD_6;                       // CCR2 toggle/set
+  TBCCR2 = 96;                              // CCR2 PWM duty cycle
+  TBCTL = TBSSEL_2 + MC_3;                  // SMCLK, up-down mode
+
+  _BIS_SR(CPUOFF);                          // Enter LPM0
+}
+
+#endif
+
+#if 0
+//*******************************************************************************
+//  MSP-FET430P140 Demo - Timer_B, PWM TB1-2, Up/Down Mode, 32kHz ACLK
+//
+//  Description: This program generates two PWM outputs on P4.1,2 using
+//  Timer_B configured for up/down mode. The value in CCR0, 128, defines the
+//  PWM period/2 and the values in CCR1 and CCR2 the PWM duty cycles. Using
+//  32kHz ACLK as TBCLK, the timer period is 7.8ms with a 75% duty cycle on
+//  P4.1 and 25% on P4.2. Normal operating mode is LPM3.
+//  ACLK = TBCLK = LFXT1 = 32768Hz, MCLK = default DCO ~800kHz.
+//  //* External watch crystal installed on XIN XOUT is required for ACLK *//	
+//
+//               MSP430F149
+//            -----------------
+//        /|\|              XIN|-
+//         | |                 | 32kHz
+//         --|RST          XOUT|-
+//           |                 |
+//           |         P4.1/TB1|--> CCR1 - 75% PWM
+//           |         P4.2/TB2|--> CCR2 - 25% PWM
+//
+//  M. Buccini / G. Morton
+//  Texas Instruments Inc.
+//  May 2005
+//  Built with Code Composer Essentials Version: 1.0
+//******************************************************************************
+
+#include  <msp430x14x.h>
+
+void main(void)
+{
+  WDTCTL = WDTPW + WDTHOLD;                 // Stop WDT
+  P4DIR |= 0x06;                            // P4.1 and P4.2 output
+  P4SEL |= 0x06;                            // P4.1 and P4.2 TB1/2 otions
+  TBCCR0 = 128;                             // PWM Period/2
+  TBCCTL1 = OUTMOD_6;                       // CCR1 toggle/set
+  TBCCR1 = 32;                              // CCR1 PWM duty cycle
+  TBCCTL2 = OUTMOD_6;                       // CCR2 toggle/set
+  TBCCR2 = 96;                              // CCR2 PWM duty cycle
+  TBCTL = TBSSEL_1 + MC_3;                  // ACLK, up-down mode
+
+  _BIS_SR(LPM3_bits);                       // Enter LPM3
+}
+
+
+#endif
+
+#if 1 //Cannot test now
+//*******************************************************************************
+//  MSP-FET430P140 Demo - Timer_B, PWM TB1-2, Up/Down Mode, HF XTAL ACLK
+//
+//  Description: This program generates two PWM outputs on P4.1,2 using
+//  Timer_B configured for up/down mode. The value in CCR0, 128, defines the
+//  PWM period/2 and the values in CCR1 and CCR1 the PWM duty cycles. Using
+//  HF XTAL ACLK as TBCLK, the timer period is HF XTAL/256 with a 75% duty
+//  cycle on P4.1 and 25% on P4.2.
+//  ACLK = MCLK = TBCLK = HF XTAL
+//  //* HF XTAL REQUIRED AND NOT INSTALLED ON FET *//
+//  //* Min Vcc required varies with MCLK frequency - refer to datasheet *//	
+//
+//               MSP430F149
+//            -----------------
+//        /|\|              XIN|-
+//         | |                 | HF XTAL (455k - 8MHz)
+//         --|RST          XOUT|-
+//           |                 |
+//           |         P4.1/TB1|--> CCR1 - 75% PWM
+//           |         P4.2/TB2|--> CCR2 - 25% PWM
+//
+//  M. Buccini / G. Morton
+//  Texas Instruments Inc.
+//  May 2005
+//  Built with Code Composer Essentials Version: 1.0
+//******************************************************************************
+
+#include  <msp430x14x.h>
+
+void main(void)
+{
+  volatile unsigned int i;
+  WDTCTL = WDTPW +WDTHOLD;                  // Stop WDT
+  BCSCTL1 |= XTS;                           // ACLK= LFXT1 = HF XTAL
+
+  do
+  {
+    IFG1 &= ~OFIFG;                         // Clear OSCFault flag
+    for (i = 0xFF; i > 0; i--);             // Time for flag to set
+  }
+  while ((IFG1 & OFIFG));                   // OSCFault flag still set?
+
+  BCSCTL2 |= SELM_3;                        // MCLK= LFXT1 (safe)
+  P4DIR |= 0x06;                            // P4.1 and P4.2 output
+  P4SEL |= 0x06;                            // P4.2 and P4.2 TB1/2 otions
+  TBCCR0 = 128;                             // PWM Period/2
+  TBCCTL1 = OUTMOD_6;                       // CCR1 toggle/set
+  TBCCR1 = 32;                              // CCR1 PWM duty cycle
+  TBCCTL2 = OUTMOD_6;                       // CCR2 toggle/set
+  TBCCR2 = 96;                              // CCR2 PWM duty cycle
+  TBCTL = TBSSEL_1 + MC_3;                  // ACLK, up-down mode
+
+  _BIS_SR(CPUOFF);                          // Enter LPM0
+}
+
+
 #endif
